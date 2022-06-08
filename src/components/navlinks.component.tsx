bec35a98fa383f7ea562374fe2c78374eaab2076
rasvanB <not.obelix@gmail.com>
2022-06-08 18:13:37 +0300

    fixed navbar bugs on light theme

--- src/components/navlinks.component.tsx
+++ src/components/navlinks.component.tsx
@@ -6,9 +6,9 @@ const NavLinks = ({ open }: { open: boolean }) => {
   return (
     <div
       className={`
-        flex bg-white dark:bg-zinc-800 absolute sm:static flex-col right-0  items-end sm:items-center sm:flex-row -z-10 ml-auto bg-opacity-70 w-full sm:w-auto transition-all duration-500 ease-in ${
+        flex bg-white dark:bg-zinc-800 absolute sm:static flex-col right-0  items-end sm:items-center sm:flex-row -z-10 ml-auto w-full sm:w-auto transition-all duration-500 ease-in ${
           open
-            ? "top-12 opacity-100 border-t-8 border-t-zinc-700"
+            ? "top-12 opacity-100 border-t-8 border-t-slate-200 dark:border-t-zinc-700"
             : "-top-52 opacity-0"
         } sm:opacity-100 pr-5 sm:pr-0 sm:border-t-0`}
     >
