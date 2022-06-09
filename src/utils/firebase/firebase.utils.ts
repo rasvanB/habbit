c67739e722468100d45a47c44323a51236a24532
rasvanB <not.obelix@gmail.com>
2022-06-09 18:23:09 +0300

    basic firebase setup

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyCU_8Lk_XYfErTC1-mI8htZ-yfp0P-b74A",
+  authDomain: "habbit-db.firebaseapp.com",
+  projectId: "habbit-db",
+  storageBucket: "habbit-db.appspot.com",
+  messagingSenderId: "572942493414",
+  appId: "1:572942493414:web:32ce0922b039aee414fd27",
+  measurementId: "G-1ML4JWKDF3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
