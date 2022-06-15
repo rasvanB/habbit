5513344a110eaa643c30c9071f223dd9ccb8df79
rasvanB <not.obelix@gmail.com>
2022-06-16 00:46:36 +0300

    played around with modal style

--- src/components/add-modal.component.tsx
+++ src/components/add-modal.component.tsx
@@ -13,13 +13,13 @@ const AddModal: FC<ModalState> = ({ isHidden, close }) => {
         isHidden ? "hidden" : "flex"
       } fixed h-screen w-screen justify-center items-center bg-black bg-opacity-60`}
     >
-      <div className="relative bg-slate-200 dark:bg-zinc-700 flex flex-col p-10 rounded-md">
+      <div className="relative bg-slate-200 dark:bg-zinc-700 flex flex-col px-10 py-5 rounded-md">
         <IonIcon
           name="close-outline"
-          className="text-2xl absolute top-3 right-4 text-white cursor-pointer"
+          className="text-2xl absolute top-2 right-3 text-white cursor-pointer"
           onClick={close}
         />
-        <div className="dark:text-white">Add a new Habit</div>
+        <div className="dark:text-white text-xl">Add a new Habit</div>
         <div className="flex justify-end items-end">
           <InputBox
             label="Name"
