a646ae19dc5f291f3615a769add0cf3482d6a733
rasvanB <not.obelix@gmail.com>
2022-06-19 21:56:56 +0300

    basic context setup

--- src/context/habit.context.tsx
+++ src/context/habit.context.tsx
@@ -1,1 +1,22 @@
-export {};
+import { createContext, ReactNode } from "react";
+
+export type HabitType = {
+  name: string;
+  color: string;
+  iconName: string;
+  iconColor: string;
+};
+
+type HabitContextType = {
+  isIconsMenuOpen: boolean;
+  setIconsMenuOpen: (openState: boolean) => void;
+  isColorsMenuOpen: boolean;
+  setColorsMenuOpen: (openState: boolean) => void;
+  habits: HabitType[];
+  addHabit: (habit: HabitType) => void;
+  removeHabit: (habit: HabitType) => void;
+};
+
+export const HabitContext = createContext<HabitContextType | null>(null);
+
+export const HabitProvider = ({ children }: { children: ReactNode }) => {};
