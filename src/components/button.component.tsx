76e8d8b0679bd6b889c669a4b7b9d69cb922c02b
rasvanB <not.obelix@gmail.com>
2022-06-21 16:27:26 +0300

    moved button styles to utils file

--- src/components/button.component.tsx
+++ src/components/button.component.tsx
@@ -1,30 +1,10 @@
 import { FC } from "react";
-type buttonStyles =
-  | "navbar-login"
-  | "navbar-signup"
-  | "hero"
-  | "submit"
-  | "add-habit"
-  | "select-icon"
-  | "select-color";
+import { buttonStyles, buttonClasses } from "../utils/styles/button-styles";
+
 type ButtonProps = {
   buttonStyle: buttonStyles;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const signupButtonStyle =
-  "bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded whitespace-nowrap";
-const loginButtonStyle =
-  "text-blue-400 font-semibold py-2 px-4 rounded hover:text-blue-600 outline outline-2 whitespace-nowrap";
-const heroButtonStyle =
-  "bg-blue-500 hover:bg-blue-600 text-white font-normal py-2 px-5 rounded-xl mt-5 whitespace-nowrap";
-const submitButton =
-  "bg-blue-500 hover:bg-blue-600 w-full text-white font-semibold py-2 px-4 rounded whitespace-nowrap mt-5";
-const addHabitButton =
-  "bg-blue-500 hover:bg-blue-600 min-w-[150px] text-white font-semibold py-2 px-4 rounded whitespace-nowrap mt-5";
-const selectIcon =
-  "dark:bg-zinc-600 bg-white outline outline-1 focus:outline-2 outline-zinc-200 dark:outline-zinc-500 dark:text-white text-zinc-800 font-semibold p-[5px] ml-5 rounded whitespace-nowrap flex items-center justify-center";
-const selectColor =
-  "dark:bg-zinc-600 bg-white outline outline-1 hover:outline-2 outline-zinc-500 font-semibold p-[5px] rounded whitespace-nowrap flex items-center justify-center";
 const Button: FC<ButtonProps> = ({ buttonStyle, children, ...otherProps }) => {
   return (
     <div>
@@ -32,19 +12,19 @@ const Button: FC<ButtonProps> = ({ buttonStyle, children, ...otherProps }) => {
         {...otherProps}
         className={
           buttonStyle === "navbar-signup"
-            ? `${signupButtonStyle}`
+            ? `${buttonClasses.signupButtonStyle}`
             : buttonStyle === "navbar-login"
-            ? `${loginButtonStyle}`
+            ? `${buttonClasses.loginButtonStyle}`
             : buttonStyle === "hero"
-            ? `${heroButtonStyle}`
+            ? `${buttonClasses.heroButtonStyle}`
             : buttonStyle === "submit"
-            ? `${submitButton}`
+            ? `${buttonClasses.submitButton}`
             : buttonStyle === "add-habit"
-            ? `${addHabitButton}`
+            ? `${buttonClasses.addHabitButton}`
             : buttonStyle === "select-icon"
-            ? `${selectIcon}`
+            ? `${buttonClasses.selectIcon}`
             : buttonStyle === "select-color"
-            ? `${selectColor}`
+            ? `${buttonClasses.selectColor}`
             : ""
         }
       >
---
+++ src/utils/styles/button-styles.tsx
@@ -0,0 +1,25 @@
+export type buttonStyles =
+  | "navbar-login"
+  | "navbar-signup"
+  | "hero"
+  | "submit"
+  | "add-habit"
+  | "select-icon"
+  | "select-color";
+
+export const buttonClasses = {
+  signupButtonStyle:
+    "bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded whitespace-nowrap",
+  loginButtonStyle:
+    "text-blue-400 font-semibold py-2 px-4 rounded hover:text-blue-600 outline outline-2 whitespace-nowrap",
+  heroButtonStyle:
+    "bg-blue-500 hover:bg-blue-600 text-white font-normal py-2 px-5 rounded-xl mt-5 whitespace-nowrap",
+  submitButton:
+    "bg-blue-500 hover:bg-blue-600 w-full text-white font-semibold py-2 px-4 rounded whitespace-nowrap mt-5",
+  addHabitButton:
+    "bg-blue-500 hover:bg-blue-600 min-w-[150px] text-white font-semibold py-2 px-4 rounded whitespace-nowrap",
+  selectIcon:
+    "dark:bg-zinc-600 bg-white outline outline-1 focus:outline-2 outline-zinc-200 dark:outline-zinc-500 dark:text-white text-zinc-800 font-semibold p-[5px] ml-5 rounded whitespace-nowrap flex items-center justify-center",
+  selectColor:
+    "dark:bg-zinc-600 bg-white outline outline-1 hover:outline-2 outline-zinc-500 font-semibold p-[5px] rounded whitespace-nowrap flex items-center justify-center",
+};
