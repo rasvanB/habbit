150fbaee435c7f38c2290c7ac909b0369ce4833a
rasvanB <not.obelix@gmail.com>
2022-06-22 22:57:10 +0300

    updated .gitignore

--- src/components/nav.component.tsx
+++ src/components/nav.component.tsx
@@ -3,6 +3,7 @@ import { signOutUser } from "../utils/firebase/firebase.utils";
 import { UserContext } from "../context/user.context";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useNavigate } from "react-router-dom";
+
 type NavProps = {
   username: string;
   photourl: string;
@@ -11,21 +12,20 @@ type NavProps = {
 const Nav: FC<NavProps> = ({ username, photourl }) => {
   const navigate = useNavigate();
   const { setLoading } = useContext(UserContext);
+
   const handleSignOut = async () => {
     await signOutUser();
     setLoading(true);
     navigate("/auth/sign-in");
   };
+
   return (
     <div className="flex items-center py-3 px-40 bg-slate-100 dark:bg-zinc-900 border-b border-slate-300 dark:border-zinc-600">
-      <div
-        onClick={handleSignOut}
-        className="w-[35px] h-[35px] outline-1 cursor-pointer"
-      >
+      <div onClick={handleSignOut} className="w-[35px] h-[35px] cursor-pointer">
         <LazyLoadImage
           src={photourl}
           referrerPolicy="no-referrer"
-          className="rounded-md outline outline-1 dark:outline-gray-700 outline-zinc-500"
+          className="rounded-md outline outline-2 outline-offset-2 outline-blue-400"
         />
       </div>
       <h1 className="ml-3 font-poppins font-medium text-zinc-800 dark:text-gray-100">
