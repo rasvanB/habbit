5f609d998643d9dc3c8c6aff6f57e9419319053a
rasvanB <not.obelix@gmail.com>
2022-06-23 17:00:40 +0300

    fixed typo

--- src/utils/modal.utils.ts
+++ src/utils/modal.utils.ts
@@ -5,5 +5,5 @@ export const validateModal = (habit: Habit) => {
   if (habit.name.length > 150) return "Name is too long (max. 150 characters)";
   if (!habit.iconName) return "Please select the icon";
   if (!habit.unit) return "Please enter the unit";
-  if (habit.unit.length > 100) return "Unit is too log (max. 100 characters)";
+  if (habit.unit.length > 100) return "Unit is too long (max. 100 characters)";
 };
