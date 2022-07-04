ed47779c21ddcb38132dc191c7b5826915a1536f
rasvanB <not.obelix@gmail.com>
2022-07-04 23:21:54 +0300

    added props

--- src/components/card/progress-menu.component.tsx
+++ src/components/card/progress-menu.component.tsx
@@ -1,4 +1,7 @@
-const ProgressMenu = () => {
+type ProgressMenuProps = {
+  isOpen: boolean;
+};
+const ProgressMenu = ({ isOpen }: ProgressMenuProps) => {
   return <div className="bg-red-500">Progress Menu</div>;
 };
 export default ProgressMenu;
