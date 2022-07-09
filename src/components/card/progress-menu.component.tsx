481d11212be0004501229960034f4feada0658e6
rasvanB <not.obelix@gmail.com>
2022-07-09 23:16:47 +0300

    added increment buttons

--- src/components/card/progress-menu.component.tsx
+++ src/components/card/progress-menu.component.tsx
@@ -6,9 +6,20 @@ const ProgressMenu = ({ isOpen }: ProgressMenuProps) => {
     <div
       className={`${
         isOpen ? "flex" : "hidden"
-      } absolute top-7 right-0 flex-col bg-neutral-800 outline outline-1 dark:outline-zinc-600 rounded-md p-2`}
+      } absolute top-8 right-0 flex-col bg-neutral-800 outline outline-1 dark:outline-zinc-600 rounded-md p-2 `}
     >
-      <div>Test</div>
+      <div className="whitespace-nowrap text-sm font-semibold dark:text-neutral-400 text-center">
+        Enter a value
+      </div>
+      <div className="flex">
+        <button className="bg-blue-400 w-[40px] rouded-md text-white text-4xl font-poppins font-thin text-center">
+          -
+        </button>
+        <input type="text" className="" />
+        <button className="bg-blue-400 w-[40px] text-white text-3xl font-poppins">
+          +
+        </button>
+      </div>
     </div>
   );
 };
