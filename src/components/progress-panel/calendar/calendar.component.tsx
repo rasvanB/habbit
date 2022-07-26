eec36dd59f3870d1ddebf2f6ffb03e5c9e4e54ab
rasvanB <not.obelix@gmail.com>
2022-07-26 12:36:06 +0300

    add dates from previous month

--- src/components/progress-panel/calendar/calendar.component.tsx
+++ src/components/progress-panel/calendar/calendar.component.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { PanelContext } from "../../../context/progress-panel.context";
-import { getDatesOfMonth } from "../../../utils/calendar.utils";
+import { getDatesOfMonth, weekDays } from "../../../utils/calendar.utils";
 import CalendarNavigation from "./calendar-nav.component";
 
 const Calendar = () => {
@@ -10,8 +10,18 @@ const Calendar = () => {
     <div>
       <CalendarNavigation />
       <div className="grid grid-cols-7 text-white text-center">
+        {weekDays.map((day) => {
+          return <div key={day}>{day.slice(0, 2)}</div>;
+        })}
+      </div>
+      <div className="grid grid-cols-7 text-center">
         {monthDates.map((date) => {
-          return <div key={date.d.getDate()}>{`${date.d.getDate()}`}</div>;
+          return (
+            <div
+              className={`${date.active ? "text-white" : "text-neutral-600"}`}
+              key={date.d.getTime()}
+            >{`${date.d.getDate()}`}</div>
+          );
         })}
       </div>
     </div>
--- src/utils/calendar.utils.ts
+++ src/utils/calendar.utils.ts
@@ -13,26 +13,66 @@ export const monthNames: { [key: number]: string } = {
   11: "December",
 };
 
+export const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 export const getMonthFromNumber = (x: number): string | undefined => {
   if (x >= 0 && x < 12) {
     return monthNames[x];
   }
 };
 
-export const getDatesOfMonth = (currentMonth: Date): { d: Date }[] => {
+export const getDatesOfMonth = (
+  currentMonth: Date
+): { d: Date; active: boolean }[] => {
+  const firstDayOfMonth = new Date(
+    currentMonth.getFullYear(),
+    currentMonth.getMonth(),
+    1
+  );
+  const firstDayOfMonthWeekDay = firstDayOfMonth.getDay();
+
+  const previousMonth = new Date(
+    currentMonth.getFullYear(),
+    currentMonth.getMonth(),
+    0
+  );
+
+  const previousMonthLastDay = new Date(
+    previousMonth.getFullYear(),
+    previousMonth.getMonth() + 1,
+    0
+  );
+  const dates: { d: Date; active: boolean }[] = [];
+
+  for (let i = 0; i < firstDayOfMonthWeekDay; i++) {
+    dates.push({
+      d: new Date(
+        previousMonth.getFullYear(),
+        previousMonth.getMonth(),
+        previousMonthLastDay.getDate() - firstDayOfMonthWeekDay + i + 1
+      ),
+      active: false,
+    });
+  }
   const lastDayOfMonth = new Date(
     currentMonth.getFullYear(),
     currentMonth.getMonth() + 1,
     0
   );
-  const dates: { d: Date }[] = [];
   for (let i = 0; i < lastDayOfMonth.getDate(); i++) {
     dates.push({
       d: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), i + 1),
+      active: true,
     });
   }
-  console.log(dates);
-
   return dates;
 };
 export const nextMonth = (currentMonth: Date): Date =>
