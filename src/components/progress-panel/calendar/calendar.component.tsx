77ea3c64c8b1d1557dcf2f5b7b6f1de9dd29847b
rasvanB <not.obelix@gmail.com>
2022-07-27 12:16:21 +0300

    active days are shown on calendar now

--- src/components/progress-panel/calendar/calendar.component.tsx
+++ src/components/progress-panel/calendar/calendar.component.tsx
@@ -1,11 +1,24 @@
-import { useContext } from "react";
+import { useCallback, useContext, useMemo } from "react";
 import { PanelContext } from "../../../context/progress-panel.context";
 import { getDatesOfMonth, weekDays } from "../../../utils/calendar.utils";
 import CalendarNavigation from "./calendar-nav.component";
 
 const Calendar = () => {
-  const { startingDate } = useContext(PanelContext);
+  const { startingDate, selectedHabit } = useContext(PanelContext);
   const monthDates = getDatesOfMonth(startingDate);
+
+  const getActiveDays = useCallback(() => {
+    return selectedHabit?.activeDays.filter((day) => day.completed);
+  }, [selectedHabit]);
+
+  const activeDays = useMemo(
+    () => getActiveDays(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [selectedHabit]
+  );
+
+  let activeDaysIndex = 0;
+
   return (
     <div>
       <CalendarNavigation />
@@ -16,9 +29,32 @@ const Calendar = () => {
       </div>
       <div className="grid grid-cols-7 text-center">
         {monthDates.map((date) => {
+          let isActiveDay = false;
+          if (activeDays) {
+            if (activeDaysIndex < activeDays.length) {
+              const activeDayAsDate = new Date(
+                activeDays[activeDaysIndex].date
+              );
+              activeDayAsDate.setMonth(activeDayAsDate.getMonth() + 1);
+              if (
+                activeDayAsDate.getDate() === date.d.getDate() &&
+                activeDayAsDate.getMonth() === date.d.getMonth() &&
+                activeDayAsDate.getFullYear() === date.d.getFullYear()
+              ) {
+                isActiveDay = true;
+                activeDaysIndex++;
+              }
+            }
+          }
           return (
             <div
-              className={`${date.active ? "text-white" : "text-neutral-600"}`}
+              className={`${
+                date.active
+                  ? isActiveDay
+                    ? "text-blue-400"
+                    : "text-white"
+                  : "text-neutral-600"
+              }`}
               key={date.d.getTime()}
             >{`${date.d.getDate()}`}</div>
           );
