bdfaafb3c1d712579ed346a51cc69a46964f1d6b
rasvanB <not.obelix@gmail.com>
2022-07-30 03:56:43 +0300

    changed text to icons

--- src/components/progress-panel/calendar/calendar-nav.component.tsx
+++ src/components/progress-panel/calendar/calendar-nav.component.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { Icon } from "@iconify/react";
 import { useCallback, useContext } from "react";
 import { PanelContext } from "../../../context/progress-panel.context";
 import {
@@ -20,14 +21,24 @@ const CalendarNavigation = () => {
 
   return (
     <div className="flex justify-between dark:text-gray-200">
-      <div onClick={handlePrevMonth}>PREV</div>
+      <div
+        onClick={handlePrevMonth}
+        className="flex items-center ml-3 bg-[rgb(48,48,48)] p-2 rounded-lg hover:bg-neutral-700"
+      >
+        <Icon icon={"ep:arrow-left-bold"} />
+      </div>
       <div className="flex gap-1">
-        <div className="text-center">{`${getMonthFromNumber(
+        <div className="text-center flex items-center">{`${getMonthFromNumber(
           startingDate.getMonth()
         )}`}</div>
-        <div className="text-center">{`${startingDate.getFullYear()}`}</div>
+        <div className="text-center flex items-center">{`${startingDate.getFullYear()}`}</div>
       </div>
-      <div onClick={handleNextMonth}>NEXT</div>
+      <div
+        onClick={handleNextMonth}
+        className="flex items-center mr-3 bg-[rgb(48,48,48)] p-2 rounded-lg hover:bg-neutral-700"
+      >
+        <Icon icon={"ep:arrow-right-bold"} />
+      </div>
     </div>
   );
 };
