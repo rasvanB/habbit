a31ad9de667df2c3a23a807a1c2c58c58bf33833
rasvanB <not.obelix@gmail.com>
2022-08-05 20:16:37 +0300

    changed pie incomplete color

--- src/components/progress-panel/chart/chart.component.tsx
+++ src/components/progress-panel/chart/chart.component.tsx
@@ -4,7 +4,7 @@ const CompletionRate = () => {
     <div className="w-[200px] h-[230px] dark:bg-zinc-800 dark:text-gray-200 rounded-lg mt-3 font-poppins p-2 relative ml-2">
       <div className="text-center font-medium mt-2">Completion Rate</div>
       <div className="w-[180px] h-[180px] relative mt-1">
-        <div className="absolute flex justify-center items-center text-2xl text-gray-200 font-medium font-poppins text-center top-0 left-0 w-full h-full">
+        <div className="absolute flex justify-center items-center text-xl text-gray-200 font-medium font-poppins text-center top-0 left-0 w-full h-full">
           80%
         </div>
         <ResponsivePie
@@ -14,7 +14,7 @@ const CompletionRate = () => {
               value: 80,
               color: "#5480d9",
             },
-            { id: 1, value: 20, color: "#eeeeee" },
+            { id: 1, value: 20, color: "#363636" },
           ]}
           innerRadius={0.5}
           padAngle={0.7}
