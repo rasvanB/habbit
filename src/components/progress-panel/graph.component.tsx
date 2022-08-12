1b06a90660b914a537489696edfb2e5f399faf46
rasvanB <not.obelix@gmail.com>
2022-08-12 16:51:04 +0300

    created function to get progress for all months as object

--- src/components/progress-panel/graph.component.tsx
+++ src/components/progress-panel/graph.component.tsx
@@ -1,15 +1,47 @@
 import { ResponsiveBar } from "@nivo/bar";
-import { useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { PanelContext } from "../../context/progress-panel.context";
 
 const Graph = () => {
   const [reload, setReload] = useState(false);
+  const { selectedHabit } = useContext(PanelContext);
+
+  const getProgressByMonth = useCallback(() => {
+    if (selectedHabit && selectedHabit.activeDays) {
+      let dataAsObj: any = {};
+      if (selectedHabit.activeDays.length > 0) {
+        for (let i = 0; i < selectedHabit.activeDays.length; i++) {
+          let month = selectedHabit.activeDays[i].date.slice(0, -3);
+          if (!dataAsObj.hasOwnProperty(month)) {
+            Object.assign(dataAsObj, {
+              [month]: selectedHabit.activeDays[i].progress,
+            });
+          } else {
+            Object.assign(dataAsObj, {
+              [month]: dataAsObj[month] + selectedHabit.activeDays[i].progress,
+            });
+          }
+        }
+        console.log(dataAsObj);
+      }
+    }
+    return null;
+  }, [selectedHabit]);
+
   useEffect(() => {
-    setInterval(() => {
-      setReload(!reload);
-    }, 700);
-  }, [reload]);
+    if (selectedHabit) {
+      setTimeout(() => {
+        setReload(true);
+        getProgressByMonth();
+      }, 1000);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reload, selectedHabit]);
   return (
-    <div className="w-[600px] h-[350px] dark:bg-zinc-800 rounded-md pt-10 px-2 mt-3">
+    <div className="w-[600px] h-[380px] dark:bg-zinc-800 rounded-md pt-2 px-2 mt-3 pb-10">
+      <div className="text-center font-medium mt-2 font-poppins dark:text-gray-200 mb-2">
+        Progress by month
+      </div>
       <ResponsiveBar
         data={[
           { month: "JAN", value: 1 },
