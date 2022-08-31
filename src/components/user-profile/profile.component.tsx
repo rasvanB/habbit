8094db80b7e8b1d039860b7fa7846d5fa755449b
rasvanB <not.obelix@gmail.com>
2022-08-31 05:01:17 +0300

    check if image is too big

--- src/components/user-profile/profile.component.tsx
+++ src/components/user-profile/profile.component.tsx
@@ -1,16 +1,37 @@
 import { Icon } from "@iconify/react";
-import { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { UserContext } from "../../context/user.context";
+import { showToast } from "../../utils/toast/habit-toasts";
 
 type ProfileProps = {
   isOpen: boolean;
   close: () => void;
 };
 
+const MB = 1048576;
+
 const Profile = ({ isOpen, close }: ProfileProps) => {
   const { currentUser } = useContext(UserContext);
+  const uploadRef = useRef<HTMLInputElement>(null);
+
   const handleClose = () => {
     close();
+  };
+
+  const handleUploadClick = () => {
+    if (uploadRef.current) {
+      uploadRef.current.click();
+    }
+  };
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const image = e.target.files[0];
+      if (image.size > 2 * MB) {
+        showToast("error", "File size too big (max. 2MB)");
+      } else {
+      }
+    }
   };
 
   return (
@@ -25,7 +46,7 @@ const Profile = ({ isOpen, close }: ProfileProps) => {
           className="p-1 text-2xl absolute top-2 right-3 dark:text-gray-200 cursor-pointer rounded-full outline outline-1 dark:outline-zinc-600 outline-zinc-300 hover:bg-gray-200 dark:hover:bg-zinc-500 bg-white dark:bg-zinc-600"
           onClick={handleClose}
         />
-        <div className="text-center">User Profile</div>
+        <div className="text-center mb-2">User Profile</div>
         <div className="w-fit h-fit relative">
           <img
             alt="profile"
@@ -33,9 +54,19 @@ const Profile = ({ isOpen, close }: ProfileProps) => {
             referrerPolicy="no-referrer"
             className="rounded-full outline outline-2 dark:outline-zinc-400"
           />
+          <input
+            type="file"
+            ref={uploadRef}
+            hidden
+            accept="image/jpeg,image/png"
+            onChange={(e) => {
+              handleUpload(e);
+            }}
+          />
           <Icon
             icon="clarity:edit-solid"
-            className="absolute bottom-[-5px] right-1 bg-blue-600 p-2 text-3xl rounded-full"
+            className="absolute bottom-[-5px] right-1 bg-blue-600 p-2 text-3xl rounded-full cursor-pointer hover:bg-blue-500"
+            onClick={handleUploadClick}
           ></Icon>
         </div>
       </div>
