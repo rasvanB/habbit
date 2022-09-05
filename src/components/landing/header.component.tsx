079ec98bdf5b4151e8f85dd42edf4e3e704b8d12
rasvanB <not.obelix@gmail.com>
2022-09-06 01:56:06 +0300

    removed ionicon

--- src/components/landing/header.component.tsx
+++ src/components/landing/header.component.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import Logo from "../other/logo.component";
 import NavLinks from "./navlinks.component";
-import IonIcon from "@reacticons/ionicons";
 import { useState } from "react";
+import { Icon } from "@iconify/react";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -17,10 +17,9 @@ const Header = () => {
         onClick={() => setOpen(!open)}
         className="w-full h-14 dark:bg-zinc-800 bg-slate-100 sm:bg-opacity-70 z-0 flex flex-row items-center justify-end select-none absolute sm:hidden right-0 dark:text-white text-zinc-700 cursor-pointer"
       >
-        <IonIcon
-          className="pr-3"
-          name={`${open ? "close-outline" : "menu-outline"}`}
-          size="large"
+        <Icon
+          className="mr-3 text-white text-3xl"
+          icon={`${open ? "heroicons-outline:x" : "gg:menu"}`}
         />
       </div>
       <NavLinks open={open} />
