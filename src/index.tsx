f64b54b2d48c4d7c795fc8ceb1735afb57880968
rasvanB <not.obelix@gmail.com>
2022-09-07 15:55:23 +0300

    created context for progress-calendar and wrapped it in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./context/theme.context";
 import UserProvider from "./context/user.context";
 import PanelProvider from "./context/progress-panel.context";
-import "./index.css";
-import App from "./App";
 import ModalProvider from "./context/add-modal.context";
+import ProgressCalendarProvider from "./context/progress-calendar.contex";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -18,9 +18,11 @@ root.render(
       <ModalProvider>
         <PanelProvider>
           <UserProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
+            <ProgressCalendarProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </ProgressCalendarProvider>
           </UserProvider>
         </PanelProvider>
       </ModalProvider>
