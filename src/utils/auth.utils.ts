9c50285596c03030d5154caf51ef0c817a3c8f50
rasvanB <not.obelix@gmail.com>
2023-02-09 20:12:28 +0200

    created zod objects for auth

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 export const validateSignIn = (email: string, password: string) => {
@@ -40,3 +41,53 @@ export const validateSignUp = (
     return "Username must be at least 4 characters";
   }
 };
+
+export const LoginScheme = z
+  .object(
+    {
+      email: z
+        .string()
+        .email({
+          message: "Invalid email",
+        })
+        .min(6),
+      password: z
+        .string()
+        .min(6, {
+          message: "Password must be at least 6 characters",
+        })
+        .max(30, {
+          message: "Password must be less than 30 characters",
+        }),
+    },
+    {
+      required_error: "Please fill out all fields",
+    }
+  )
+  .required();
+
+export const SignUpScheme = LoginScheme.extend({
+  confirmPassword: z
+    .string()
+    .min(6, {
+      message: "Password must be at least 6 characters",
+    })
+    .max(30, {
+      message: "Password must be less than 30 characters",
+    }),
+  username: z
+    .string()
+    .min(4, {
+      message: "Username must be at least 4 characters",
+    })
+    .max(16, {
+      message: "Username must be less than 16 characters",
+    }),
+}).superRefine((data) => {
+  if (data.password !== data.confirmPassword) {
+    return {
+      path: ["confirmPassword"],
+      message: "Passwords do not match",
+    };
+  }
+});
