8fdba40c85272607e7fdf51e4f0b63ef8b4eee9d
rasvanB <not.obelix@gmail.com>
2023-02-17 23:09:22 +0200

    added property for edit mode

--- src/utils/store/calendar.store.ts
+++ src/utils/store/calendar.store.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 type CalendarStore = {
   selectedDate: Date;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
   setSelectedDate: (d: Date) => void;
 };
 
 export const useCalendarStore = create<CalendarStore>((set) => ({
   selectedDate: new Date(),
+  editMode: false,
+  setEditMode: (editMode: boolean) => set({ editMode }),
   setSelectedDate: (d: Date) => set({ selectedDate: d }),
 }));
