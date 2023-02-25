0e5e5942578de09813ed65103954349cf3b047fd
rasvanB <not.obelix@gmail.com>
2023-02-25 21:22:15 +0200

    edit button basic setup

--- src/components/progress-panel/calendar/calendar.component.tsx
+++ src/components/progress-panel/calendar/calendar.component.tsx
@@ -1,13 +1,16 @@
+import { useCalendarStore } from "../../../utils/store/calendar.store";
 import CalendarNavigation from "./calendar-nav.component";
 import DatesContainer from "./dates-container.component";
 import DayLabels from "./day-labels.component";
 
 const Calendar = () => {
+  const setEditMode = useCalendarStore((state) => state.setEditMode);
   return (
     <div className="dark:bg-zinc-800 w-fit dark:text-gray-200 bg-white shadow-sm p-4 rounded-lg mt-3 font-poppins min-w-[348px] ">
       <CalendarNavigation />
       <DayLabels />
       <DatesContainer />
+      <button>EDIT</button>
     </div>
   );
 };
